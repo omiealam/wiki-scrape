d3cb89d9ef3d5bdfc68e76e3eaab60479eea98c6
Omar <ramoalam@live.com>
2023-03-06 10:15:34 +0100

    Logic for constructing and performing Download requests

--- app/controllers/downloads_controller.rb
+++ app/controllers/downloads_controller.rb
@@ -1,22 +1,80 @@
 class DownloadsController < ApplicationController
+  include ActiveStorage::SendZip
 
   def create
-    page = Wikipedia.find(download_params[:input])
+    @page = Wikipedia.find(create_params[:input])
 
-    if(download_params[:is_pro] == "true")
-       # TODO: Handle illegal access of pro settings (fail safely)
-      authenticate_user!
-      # TODO: Make sure user is paying customer
-      puts download_params
-      Download.create(page_url: page.fullurl, page_title: page.title, belongs_to_pro: true, user_id: current_user.id, text_requested: download_params[:text_requested], images_requested: download_params[:images_requested], links_requested: download_params[:links_requested])
+    clear_old_downloads
+
+    if page_nil?
+      redirect_to root_path, alert: "That page doesn't seem to exist!"
     else
-      Download.create(page_url: page.fullurl, page_title: page.title, belongs_to_pro: false, text_requested: download_params[:text_requested])
+      if(create_params[:is_pro] == "true")
+        authenticate_user!
+        unless current_user.paying_customer
+          redirect_to "/account", alert: "Activate your account to access Pro features"
+        else
+          create_paid_download
+        end
+      else
+        create_free_download
+      end
+    end
+  end
+
+  def show
+    # TODO: Make sure people can only access their own items
+    @download = Download.find_by(id: show_params[:id])
+    respond_to do |format|
+      format.zip { send_zip construct_download, filename: "#{@download.page_title}.zip"}
     end
   end
 
   private
 
-  def download_params
+  def create_params
     params.require(:download).permit(:input, :is_pro, :text_requested, :images_requested, :links_requested)
+  end
+
+  def show_params
+    params.permit(:id, :format)
+  end
+
+  def construct_download
+    output = {}
+    output["#{@download.page_title}"] = {}
+    inner_file = output["#{@download.page_title}"]
+    inner_file[:text_file] = @download.text_file if @download.text_requested
+    inner_file[:links_csv] = @download.links_csv if @download.links_requested
+    inner_file[:images] = construct_image_download if @download.images_requested
+    return output
+  end
+
+  def construct_image_download
+    arr = []
+    @download.images.each {|image| arr << image}
+    arr
+  end
+
+  def page_nil?
+    @page.text.nil?
+  end
+
+  def create_free_download
+    @download = Download.create(page_url: @page.fullurl, page_title: @page.title, belongs_to_pro: false, text_requested: create_params[:text_requested])
+    # TODO: Make this a delayed job with callback to redirect to download link
+    @download.perform_download
+    redirect_to rails_blob_path(@download.text_file, disposition: "attachment", filename: "#{@download.page_title}.txt")
+  end
+
+  def create_paid_download
+    @download = Download.create(page_url: @page.fullurl, page_title: @page.title, belongs_to_pro: true, user_id: current_user.id, text_requested: create_params[:text_requested], images_requested: create_params[:images_requested], links_requested: create_params[:links_requested])
+    @download.delay.perform_download
+    redirect_to "/dashboard", notice: "That page has been added to the download queue"
+  end
+
+  def clear_old_downloads
+    # TODO: Use the task delete_free_downloads once I understand Heroku pricing
+    Download.where("belongs_to_pro = ?", false).where('created_at <= ?', 5.minutes.ago).destroy_all
   end
 end
--- app/models/download.rb
+++ app/models/download.rb
@@ -1,2 +1,49 @@
 class Download < ApplicationRecord
+  has_one_attached :text_file
+  has_one_attached :links_csv
+  has_many_attached :images
+  has_one_attached :output_folder
+
+  def perform_download
+    # Create if statements for various cases
+    perform_text_download if self.text_requested
+    perform_images_download if self.images_requested
+    perform_links_download if self.links_requested
+    #construct_download
+    self.update(download_completed: true)
+  end
+
+  private
+
+  def perform_text_download
+    t = Tempfile.new
+    page = Wikipedia.find(self.page_url)
+    File.open(t, 'w') { |file| file.write("= #{page.title.upcase} =" +"\n" +"\n" + page.text) }
+    self.text_file.attach(io: File.open(t), filename: "#{self.page_title}.txt", content_type: "application/txt")
+    t.close
+    t.unlink
+  end
+
+  def perform_links_download
+    t = Tempfile.new
+    page = Wikipedia.find(self.page_url)
+    CSV.open(t, "w") do |csv|
+      page.extlinks.each { |link| csv << [link] }
+    end
+    self.links_csv.attach(io: File.open(t), filename: "#{self.page_title}_links.csv", content_type: "application/csv")
+    t.close
+    t.unlink
+  end
+
+  def perform_images_download
+    page = Wikipedia.find(self.page_url)
+    page.image_urls.each do |url|
+      t = Tempfile.new
+      download = URI.open(url)
+      IO.copy_stream(download, t)
+      self.images.attach(io: File.open(t), filename: "#{download.base_uri.to_s.split('/')[-1]}", content_type: "application/image")
+      t.close
+      t.unlink
+    end
+  end
 end
